4d8c4b50e1c8e64711d6ca8db444b2cffa8a435e
Kevin Dodokal <dodokalkev@gmail.com>
2026-02-04 20:40:30 +0100

    feat: implémentation de la suppression des travaux dans la modale

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
@@ -89,6 +89,7 @@ getWorks();
 const token = localStorage.getItem("token");
 
 // SI ON EST CONNECTÉ (Token présent)
+
 if (token) {
     
     // 1. Création de la barre noire
@@ -129,5 +130,123 @@ if (token) {
         editLink.classList.add("edit-btn"); // On ajoute la classe CSS
         editLink.innerHTML = '<i class="fa-regular fa-pen-to-square"></i> modifier';
         titleProjects.appendChild(editLink);
+  
+
+    // === GESTION DE LA MODALE ===
+    const modal = document.querySelector("#modal");
+    const closeModalBtn = document.querySelector(".js-close-modal");
+
+    // 1. OUVRIR LA MODALE : Au clic sur "modifier"
+    editLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        modal.classList.add("modal-show"); // On ajoute la classe qui affiche
+        genererGalerieModale(works);
+    });
+
+    // Fonction pour générer la galerie dans la modale
+function genererGalerieModale(projets) {
+    const modalGallery = document.querySelector(".modal-gallery");
+    modalGallery.innerHTML = ""; // On vide la galerie
+
+    for (let i = 0; i < projets.length; i++) {
+        const projet = projets[i];
+
+        // 1. Création de la balise figure
+        const figure = document.createElement("figure");
+        figure.style.position = "relative";
+
+        // 2. Création de l'image
+        const image = document.createElement("img");
+        image.src = projet.imageUrl;
+        image.alt = projet.title;
+        image.style.width = "78px";
+
+        // 3. Création de l'icône poubelle
+        const trashIcon = document.createElement("i");
+        trashIcon.classList.add("fa-solid", "fa-trash-can", "trash-icon");
+        trashIcon.id = projet.id; // Important pour la suppression future
+
+        // 4. Assemblage (Juste l'image et l'icône)
+        figure.appendChild(image);
+        figure.appendChild(trashIcon);
+        modalGallery.appendChild(figure);
     }
 }
+
+    // 2. FERMER LA MODALE : Au clic sur la croix
+    closeModalBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        modal.classList.remove("modal-show"); // On retire la classe
+    });
+
+    // 3. FERMER LA MODALE : Au clic sur le fond gris
+    modal.addEventListener("click", function(event) {
+        // Si on clique précisément sur le fond gris (modal) et pas sur la boîte blanche
+        if (event.target === modal) {
+            modal.classList.remove("modal-show");
+        }
+    });
+
+    }  
+}
+
+// Fonction pour générer la galerie dans la modale
+function genererGalerieModale(liste) {
+    const modalGallery = document.querySelector(".modal-gallery");
+    modalGallery.innerHTML = ""; // On vide la galerie
+
+    for (let i = 0; i < liste.length; i++) {
+        const projet = liste[i];
+
+        // 1. Création de la balise figure
+        const figure = document.createElement("figure");
+        figure.style.position = "relative";
+
+        // 2. Création de l'image
+        const image = document.createElement("img");
+        image.src = projet.imageUrl;
+        image.alt = projet.title;
+        image.style.width = "78px";
+
+        // 3. Création de l'icône poubelle
+        const trashIcon = document.createElement("i");
+        trashIcon.classList.add("fa-solid", "fa-trash-can", "trash-icon");
+        trashIcon.id = projet.id; // Important pour la suppression future
+
+        trashIcon.addEventListener("click" , async function (event) {
+            event.preventDefault();
+            
+            //Confirmation de la suppression
+
+            const confirmation = confirm("Voulez-vous vraiment supprimer ce projet ?")
+            if (!confirmation){
+                return;
+            }
+
+            const id = trashIcon.id;
+            const token = localStorage.geitItem("token");
+
+            // Demande de suppression à l' API
+            const response = await fetch(`http://localhost:5678/api/works/${id}`, {
+                method: "DELETE",
+                headers: {
+                    "Authorization" : `Bearer ${token}`,
+                    "Content-Type": "application/json"
+                }
+            });
+
+            if(response.ok) {
+                figure.remove();
+            } else {
+                alert ("Erreur lors de la suppression");
+            }
+        });
+
+        // 4. Assemblage (Juste l'image et l'icône)
+        figure.appendChild(image);
+        figure.appendChild(trashIcon);
+        modalGallery.appendChild(figure);
+    }
+}
+
+
